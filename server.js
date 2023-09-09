bc6eabb6da4be80bc603c0df63e3a1336a9cceed
Anshul Maurya <anshul2102@hotmail.com>
2023-09-10 00:04:38 +0530

    created msg object

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
 
 const app = express();
 const server = http.createServer(app);
@@ -10,22 +11,23 @@ const io = socketio(server);
 //set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+const botName = "ChatRoom Bot";
 //Run when a client connects
 io.on("connection", (socket) => {
   //Welcome current user
-  socket.emit("message", "Welcome to ChatRoom");
+  socket.emit("message", formatMessage(botName, "Welcome to ChatRoom"));
 
   //Broadcast when a user connects
   socket.broadcast.emit("message", "A user has joined a chat");
 
   //Runs when a client disconnects
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left the chat");
+    io.emit("message", formatMessage(botName, "A user has left the chat"));
   });
 
   //Listen for chatMessage
   socket.on("chatMessage", (msg) => {
-    io.emit("message", msg);
+    io.emit("message", formatMessage("USER", msg));
   });
 });
 
